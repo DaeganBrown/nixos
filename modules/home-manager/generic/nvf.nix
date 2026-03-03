09a6533a36901342094c2123dcf1a54b9e0a7b72
Ozymandias <149893841+DaeganBrown@users.noreply.github.com>
2026-03-03 18:38:18 -0500

    NVF setup, gruvbox, autocomplete, some languages

--- modules/home-manager/generic/nvf.nix
+++ modules/home-manager/generic/nvf.nix
@@ -4,7 +4,28 @@
   programs.nvf = {
     enable = true;
     settings = {
-      #
+      vim = {
+        options = {
+          tabstop = 2;
+          shiftwidth = 2;
+        };
+        theme = {
+          enable = true;
+          name = "gruvbox";
+          style = "dark";
+        };
+        lsp.enable = true;
+        languages = {
+          enableTreesitter = true;
+          nix.enable = true;
+          rust.enable = true;
+        };
+      
+
+        statusline.lualine.enable = true;
+        telescope.enable = true;
+        autocomplete.nvim-cmp.enable = true;
+      };
     };
   };
 }
