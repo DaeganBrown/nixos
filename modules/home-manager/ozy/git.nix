1e7b632f712731e7ffba2f38ff702e2b848ee770
Ozymandias <149893841+DaeganBrown@users.noreply.github.com>
2026-03-03 16:44:17 -0500

    Fixed, hopefully, the gpg issues for now

--- modules/home-manager/ozy/git.nix
+++ modules/home-manager/ozy/git.nix
@@ -4,7 +4,7 @@
     git = {
       enable = true;
       settings = {
-        commit.gpgsign = true;
+        # commit.gpgsign = true;
 	gpg.format = "ssh";
 	user = {
 	  signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPLxsbOscVdGqK7IaaJgld2FRYFwbsncG89ahrK90fBj 149893841+DaeganBrown@users.noreply.github.com";
