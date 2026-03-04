56beb807d5d950b230db1eca8f5cc04c187038c4
Ozymandias <149893841+DaeganBrown@users.noreply.github.com>
2026-03-04 00:46:14 -0500

    Extended hyprland ctrls

--- modules/home-manager/generic/hyprland-ctrls.nix
+++ modules/home-manager/generic/hyprland-ctrls.nix
@@ -10,24 +10,38 @@
 
 
       bind = [
-	"$mod, RETURN, exec, $terminal"
-	"$mod, S, exec, $menu"
-      ]
-      ++ (
-	## workspaces
-	builtins.concatLists ( builtins.genList (i:
-	  let ws = i + 1;
-	  in [
-	    "$mod, code:1${toString i}, workspace, ${toString ws}"
-	    "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-	  ]
-	)
+	      "$mod, RETURN, exec, $terminal"
+	      "$mod, S, exec, $menu"
+        "$mod, V, togglefloating,"
+      ] ++ (
+	      ## workspaces
+	      builtins.concatLists ( 
+          builtins.genList (i:
+	          let ws = i + 1;
+	          in [
+	            "$mod, code:1${toString i}, workspace, ${toString ws}"
+	            "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+	          ]
+	        )
         9)
       );
+
+      bindm = [
+        "$mod, mouse:272, moveWindow"
+        "$mod, mouse:273, resizeWindow"
+      ];
+
+      bindel = [
+        ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
+        ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ",XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 set 5%+"
+        ",XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-"
+      ];
+      extraConfig = ''
+        exec-once = waybar &
+        exec-once = swww &
+      '';
     };
-    extraConfig = ''
-    exec-once = waybar &
-    exec-once = swww &
-    '';
   };
 }
