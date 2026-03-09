84b0be90afc7fd80495ce0070d8cec63f44a9b9c
Ozymandias <149893841+DaeganBrown@users.noreply.github.com>
2026-03-09 16:12:52 -0500

    Added spysi configuration and home

--- hosts/spysi/configuration.nix
+++ hosts/spysi/configuration.nix
@@ -3,7 +3,14 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      ./hardware-configuration.nix 
+      ../../modules/nixos/common-pkgs.nix
+      ../../modules/nixos/multi-media.nix 
+      ../../modules/nixos/fonts.nix
+      ../../modules/nixos/steam.nix 
+      ../../modules/nixos/nvidia.nix
+      ../../modules/nixos/discord.nix
+      ../../modules/nixos/gdm.nix
     ];
 
   # Bootloader.
@@ -11,13 +18,13 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   networking.hostName = "nixos"; # Define your hostname.
   networking.networkmanager.enable = true;
 
-  time.timeZone = "America/Indiana/Vincennes";
+  time.timeZone = "America/New_York";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
@@ -33,6 +40,20 @@
     LC_TELEPHONE = "en_US.UTF-8";
     LC_TIME = "en_US.UTF-8";
   };
+  
+  # hardware
+  hardware.graphics.enable = true;
+
+  # nvidia things
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia.open = true;
+  # hardware.nvidia.prime.offload.enable = false;
+  # hardware.nvidia.prime.sync.enable = false;
+
+  # hyprland
+  programs.hyprland.enable = true;
+  programs.hyprland.package = inputs.hyprland.packages."${pkgs.system}".hyprland;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -48,9 +69,10 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    jack.enable = true;
   };
 
-  Define a user account. Don't forget to set a password with ‘passwd’.
+  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.spysi = {
     isNormalUser = true;
     description = "spysi";
@@ -65,14 +87,23 @@
       "spysi" = import ./home.nix;
     };
   };
-
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    mako
+    libnotify
+    swww
+
   ];
+  
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+  };
 
   system.stateVersion = "25.11"; # Did you read the comment?
 
