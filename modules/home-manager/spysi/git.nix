506df398282a0ee649db97881cff06763988e1c9
Spysi <266811806+SpysiBrown@users.noreply.github.com>
2026-03-09 22:31:15 -0400

    working dad PC

--- modules/home-manager/spysi/git.nix
+++ modules/home-manager/spysi/git.nix
@@ -7,9 +7,9 @@
         # commit.gpgsign = true;
 	gpg.format = "ssh";
 	user = {
-	  signingkey = "";
-	  email = "";
-	  name = "";
+	  signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDotZKr4AiIHdA7QRGXZh2fUpdUy2VxuWyWUddnVIRFk 266811806+SpysiBrown@users.noreply.github.com";
+	  email = "266811806+SpysiBrown@users.noreply.github.com";
+	  name = "Spysi";
 	};
 	init.defaultBranch = "main";
       };
