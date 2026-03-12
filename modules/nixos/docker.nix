99616854f6d075bfd07ed4a064b2ab34d057429f
Browncrashpad <149893841+DaeganBrown@users.noreply.github.com>
2026-03-11 21:46:56 -0500

    docker nix file

---
+++ modules/nixos/docker.nix
@@ -0,0 +1,13 @@
+{ pkgs, config, ... }:
+
+{
+  virtualisation.docker = {
+    enable = true;
+    enableOnBoot = true;
+    users.users."${config.username}".extraGroups = [ "docker" ];
+
+    environment.systemPackages = with pkgs; [
+      docker-compose
+    ];
+  };
+}
