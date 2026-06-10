9036ef3d2bfd8d90494606f45b7dacf93f15dcdf
Ozymandias <149893841+DaeganBrown@users.noreply.github.com>
2026-06-10 15:13:18 -0500

    Reverted from latest for capps and spysi to match nvidia

--- hosts/spysi/configuration.nix
+++ hosts/spysi/configuration.nix
@@ -19,7 +19,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   networking.hostName = "nixos"; # Define your hostname.
