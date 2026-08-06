de310c656c9121fb28abfcd2cc2092678c16c309
Ozymandias <149893841+DaeganBrown@users.noreply.github.com>
2026-08-06 14:53:10 -0500

    feat: Added xbox-app.nix to use xbox services.
    Webpage added as a desktop entry, same method as betaflight.

---
+++ modules/nixos/xbox-app.nix
@@ -0,0 +1,27 @@
+{ pkgs, config, ... }:
+{
+  ####################################
+  # Home Manager: launcher + desktop entry
+  ####################################
+  home-manager.users."${config.username}" = { pkgs, ... }: {
+    home.packages = [
+      (pkgs.writeShellScriptBin "xbox-app" ''
+        exec ${pkgs.chromium}/bin/chromium \
+          --app=https://www.xbox.com/play \
+          --name=Xbox \
+          --class=Xbox \
+          --user-data-dir="$HOME/.config/xbox-chrome-profile" \
+          --use-fake-ui-for-media-stream \
+          "$@"
+      '')
+    ];
+    xdg.desktopEntries.xbox-app = {
+      name = "Xbox";
+      genericName = "Xbox Cloud Gaming & Party Chat";
+      exec = "xbox-app";
+      icon = "chromium";
+      categories = [ "Game" ];
+      terminal = false;
+    };
+  };
+}
