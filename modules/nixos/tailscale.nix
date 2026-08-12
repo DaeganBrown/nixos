cee15fed3ecda7aaecf9a9b378b2c90fc7490611
Ozymandias <149893841+DaeganBrown@users.noreply.github.com>
2026-08-12 14:32:16 -0500

    fix: Made tailscale follow unstable
    Tailscale security patches need to be as up to date as possible,
    so making tailscale use unstable instead.

--- modules/nixos/tailscale.nix
+++ modules/nixos/tailscale.nix
@@ -1,6 +1,6 @@
-{ config, ... }:
-
+{ config, pkgs, inputs, ... }:
 { 
   services.tailscale.enable = true;
+  services.tailscale.package = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system}.tailscale;
   networking.firewall.allowedUDPPorts = [ config.services.tailscale.port ];
 }
